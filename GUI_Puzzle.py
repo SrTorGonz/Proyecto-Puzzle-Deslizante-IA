7cf5791a80e1f7e1aef4c86fee99e09f5f1a9180
Daniel Torres <yunior1941@gmail.com>
2025-03-15 21:50:39 -0500

    Ajuste en el puzzle de imagenes para que se recorte la imagen

--- GUI_Puzzle.py
+++ GUI_Puzzle.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QComboBox, QPushButton, QVBoxLayout, QHBoxLayout, QRadioButton, QGroupBox, QGridLayout
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import Qt, QSize
 import sys
 
@@ -17,7 +17,7 @@ class PuzzleConfigGUI(QWidget):
         # Mostrar el puzzle con un layout fijo
         self.puzzle_widget = QWidget()
         self.puzzle_grid = QGridLayout(self.puzzle_widget)
-        self.puzzle_grid.setSpacing(10)
+        self.puzzle_grid.setSpacing(2)
         self.puzzle_grid.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
         # Selección del tamaño del puzzle
@@ -62,7 +62,7 @@ class PuzzleConfigGUI(QWidget):
         """Inicializa la matriz del puzzle y la interfaz gráfica."""
         size = int(self.size_combo.currentText().split('x')[0])
         self.size = size
-        self.grid_state = [[i * size + j + 1 for j in range(size)] for i in range(size)]
+        self.grid_state = [[(i, j) for j in range(size)] for i in range(size)]
         self.grid_state[size - 1][size - 1] = None  # Última posición vacía
 
         self.empty_tile = (size - 1, size - 1)  # Última posición vacía
@@ -81,6 +81,10 @@ class PuzzleConfigGUI(QWidget):
         font_size = max(10, tile_size // 3)
         use_numbers = self.numbers_radio.isChecked()
 
+        image = QImage("prueba.png") if not use_numbers else None
+        piece_width = image.width() // self.size if image else 0
+        piece_height = image.height() // self.size if image else 0
+
         for i in range(self.size):
             for j in range(self.size):
                 if self.grid_state[i][j] is None:
@@ -92,10 +96,12 @@ class PuzzleConfigGUI(QWidget):
                 label.setStyleSheet(f"background-color: gray; border: 2px solid black; font-size: {font_size}px;")
 
                 if use_numbers:
-                    label.setText(str(self.grid_state[i][j]))
+                    label.setText(str(self.grid_state[i][j][0] * self.size + self.grid_state[i][j][1] + 1))
                 else:
-                    image = QPixmap("prueba.png").scaled(tile_size, tile_size, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
-                    label.setPixmap(image)
+                    original_x, original_y = self.grid_state[i][j]
+                    cropped_piece = image.copy(original_y * piece_width, original_x * piece_height, piece_width, piece_height)
+                    pixmap = QPixmap.fromImage(cropped_piece).scaled(tile_size, tile_size, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+                    label.setPixmap(pixmap)
                 
                 label.mousePressEvent = self.create_mouse_press_event(i, j)
                 self.tiles[(i, j)] = label
